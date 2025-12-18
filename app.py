596c57eb93624ee04f0df97949f70416e475dfdd
dom-edu <84454639+dom-edu@users.noreply.github.com>
2025-12-18 11:35:27 -0500

    added dropdown component

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from dash import Dash 
+from dash import Dash, html , dcc 
 
 
 # instantiate dash
 app = Dash() 
 
+# COMPONENTS 
+dd1 = dcc.Dropdown(['New York', 'Los Angeles', 'Chicago'], 
+                   placeholder="Select city...",
+                   id="dd-city-sel",
+                   multi=True)
+
 # add layout
 app.layout = [
-    
+    dd1
 ]
 
 if __name__ == '__main__':
